885ff280b9ffb5836d246ea589c007fb1d348587
Satisfaction- <76633966+Satisfaction-Sk@users.noreply.github.com>
2022-09-22 19:07:29 +0200

    UPT

--- index.js
+++ index.js
@@ -18,3 +18,7 @@ $('#filters li').click(function(){
         
 	});
 });
+const container = document.querySelector('.container');
+document.querySelector('.slider').addEventListener('input', (e) => {
+  container.style.setProperty('--position', `${e.target.value}%`);
+});
