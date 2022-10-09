a9edf50ba52b4565565a699dab1bb7e775e33fc5
ToFranck <73122097+ToFranck@users.noreply.github.com>
2022-10-09 21:45:47 +0200

    ouioui

--- index.js
+++ index.js
@@ -22,6 +22,41 @@ $('#filters li').click(function(){
 });
 
 
+
 $('#filters li').click( function() {
 	$('.home').css('display', 'none');
-  } );
+} );
+
+
+
+
+// ------------------ slider ------------------
+
+var slideIndex = [1, 1, 1, 1, 1, 1, 1, ];
+var slideId = ["mySlides1", "mySlides2","mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7" ]
+showDivs(1, 0);
+showDivs(1, 1);
+showDivs(1, 2);
+showDivs(1, 3);
+showDivs(1, 4);
+showDivs(1, 5);
+showDivs(1, 6);
+
+function plusDivs(n, no) {
+  showDivs(slideIndex[no] += n, no);
+}
+
+function showDivs(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {
+    slideIndex[no] = 1
+  }
+  if (n < 1) {
+    slideIndex[no] = x.length
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no] - 1].style.display = "block";
+} 
